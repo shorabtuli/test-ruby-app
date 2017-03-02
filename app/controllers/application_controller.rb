3a12f129aeb6fc53e767aa7c3d7e41f4f115538e
Shorab Tuli <shorabtuli@gmail.com>
2017-03-02 12:12:54 -0500

    updated authenticate: to create a new user-7

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -15,8 +15,9 @@ class ApplicationController < ActionController::Base
     rescue Stripe::InvalidRequestError
     end
   elsif params[:createNewUser].present?
+    mobileAppCustomerID = params[:mobileAppCustomerID]
     begin
-      @customer = Stripe::Customer.create(:description => "iOS SDK example customer2")
+      @customer = Stripe::Customer.create(:description => mobileAppCustomerID)
     rescue Stripe::InvalidRequestError
     end
     session[:customer_id] = @customer.id
