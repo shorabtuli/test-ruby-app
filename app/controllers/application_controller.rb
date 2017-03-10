6a6b5587c40d5fe8e0d82e67d45fbaea1db05cb2
Shorab Tuli <shorabtuli@gmail.com>
2017-03-10 18:25:41 -0500

    added the list to skip authentication of token

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,9 +3,11 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+  skip_before_action  :verify_authenticity_token
+
   def authenticate!
   # This code simulates "loading the Stripe customer for your current session".
-  # Your own logic will likely look very different.
+  # Your own logic will likely look very different. ABC
   return @customer if @customer
   if params[:customer_id].present?
     customer_id = params[:customer_id]
